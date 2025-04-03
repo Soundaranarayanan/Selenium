059d9e540ce196b6a5ffe348f979dbcb74437e21
Soundaranarayanan <2k21eee65@kiot.ac.in>
2025-04-03 12:32:18 +0530

    new

--- OrangeHrm/src/test/java/runner/RunnerOrangeHrm.java
+++ OrangeHrm/src/test/java/runner/RunnerOrangeHrm.java
@@ -3,7 +3,7 @@ package runner;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(plugin = {"pretty" , "json:target/cucumber-Report/LoginOrangeHRMTestNGReport.json"}
+@CucumberOptions(plugin = {"pretty" , "json:target/cucumber-Report/CucumberDemo1.json"}
 				,features ="src/test/resources/com/features/LognDataTable.feature"
 				,glue ="StepDefinitionDataTable"
 		)
